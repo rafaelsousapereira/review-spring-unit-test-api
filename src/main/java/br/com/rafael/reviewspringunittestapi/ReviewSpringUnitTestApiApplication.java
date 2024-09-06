d19297f112c091a31b1f4c8278bd5f85b27e911e
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-09-06 17:11:34 -0300

    feat: initialize project

---
+++ src/main/java/br/com/rafael/reviewspringunittestapi/ReviewSpringUnitTestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.reviewspringunittestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewSpringUnitTestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewSpringUnitTestApiApplication.class, args);
+    }
+
+}
