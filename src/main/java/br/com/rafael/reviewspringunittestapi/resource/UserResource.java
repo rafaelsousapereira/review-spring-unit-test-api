e392b580bdcdd9d2344234324cdedf955354e1c2
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-09-06 18:23:58 -0300

    feat: add class user repository findById

--- src/main/java/br/com/rafael/reviewspringunittestapi/resource/UserResource.java
+++ src/main/java/br/com/rafael/reviewspringunittestapi/resource/UserResource.java
@@ -1,19 +1,25 @@
 package br.com.rafael.reviewspringunittestapi.resource;
 
 import br.com.rafael.reviewspringunittestapi.domain.User;
+import br.com.rafael.reviewspringunittestapi.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class UserResource {
 
+    private final UserService userService;
+
     @GetMapping("/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id) {
+        var result = this.userService.findById(id);
 
-        return ResponseEntity.ok().body(new User(1, "Rafael Sousa", "rafael", "rafael.sousa@gmail.com", "p123"));
+        return ResponseEntity.ok().body(result);
     }
 }
