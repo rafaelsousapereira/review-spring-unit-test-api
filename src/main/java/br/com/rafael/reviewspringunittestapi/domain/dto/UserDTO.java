3b4c1a46e0b6a7f558c657b6c40fe3fc2f2cd510
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2024-09-06 22:01:40 -0300

    feat: returned user resource dto

---
+++ src/main/java/br/com/rafael/reviewspringunittestapi/domain/dto/UserDTO.java
@@ -0,0 +1,21 @@
+package br.com.rafael.reviewspringunittestapi.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+    private String name;
+    private String username;
+    private String email;
+
+    @JsonIgnore
+    private String password;
+}
--- src/main/java/br/com/rafael/reviewspringunittestapi/resource/UserResource.java
+++ src/main/java/br/com/rafael/reviewspringunittestapi/resource/UserResource.java
@@ -1,8 +1,9 @@
 package br.com.rafael.reviewspringunittestapi.resource;
 
-import br.com.rafael.reviewspringunittestapi.domain.User;
+import br.com.rafael.reviewspringunittestapi.domain.dto.UserDTO;
 import br.com.rafael.reviewspringunittestapi.services.UserService;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,11 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserResource {
 
+    private final ModelMapper mapper;
     private final UserService userService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<User> findById(@PathVariable Integer id) {
-        var result = this.userService.findById(id);
+    public ResponseEntity<UserDTO> findById(@PathVariable Integer id) {
+        var user = this.userService.findById(id);
+        var result = mapper.map(user, UserDTO.class);
 
         return ResponseEntity.ok().body(result);
     }
